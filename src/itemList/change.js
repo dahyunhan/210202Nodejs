12777a4e50ef7a67dc7ebad2174417ef7f4e1cd6
dollae <dollaeboss@gmail.com>
2022-01-13 14:33:05 +0900

    temp

---
+++ src/itemList/change.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function change() {
+    return (
+        <div>
+            <h3>상품 변경 화면</h3>
+            <p>추가</p>
+            <p>수정</p>
+            <p>삭제</p>
+           
+
+          테이블 출력
+        </div>
+    );
+}
+
+export default change;
