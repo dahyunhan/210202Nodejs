9b36a9deaa61bb2d781052f3b5e0d35a87bf5f8d
dollae <dollaeboss@gmail.com>
2022-01-16 13:25:03 +0900

    server

---
+++ config.js
@@ -0,0 +1,12 @@
+exports.config = {
+    db: {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'dollae',
+        password: '1234',
+        port: 5432,
+    },
+    server: {
+        port: 5000,
+    }
+};
